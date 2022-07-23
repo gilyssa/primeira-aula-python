8664cb99d0c43e32fe3642a1806872de301854db
gilyssa <gilyssabrito@gmail.com>
2022-07-23 15:59:33 -0300

    start

---
+++ Básico.py
@@ -0,0 +1,25 @@
+#comentário
+#print("Hello World") é pra não dar azar
+aprendiz = 'Lyssa "lima" '
+
+
+#Operaçoes aritimeticas
+result = 10 + 30
+result = 40 - 10
+result = 50 * 5
+result = 16 / 4
+result = 25 % 2
+result = 5 ** 3
+
+#+para adição
+#-para subtração
+#*para multiplicação
+#/para divisão
+#%para módulo (retorna o restante)
+#**para exponenciação
+
+
+
+
+
+
